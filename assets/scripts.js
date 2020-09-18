dd4ae5dbf0cbe4cbada5b14f114ae380b40244f6
Andrea Gulino <andrea.gulino@polimi.it>
2020-09-18 02:18:01 +0200

    first commit

---
+++ assets/scripts.js
@@ -0,0 +1,36 @@
+// toggle up and down button and manage expansion
+function togglePublications(){
+    $('#publications').toggleClass('expanded');
+    if($('#togglePubButton').hasClass('fa-chevron-down')) {
+        $('#togglePubButton').removeClass('fa-chevron-down');
+        $('#togglePubButton').addClass('fa-chevron-up');
+    } else {
+        $('#togglePubButton').removeClass('fa-chevron-up');
+        $('#togglePubButton').addClass('fa-chevron-down');
+    }
+
+}
+
+// setup the slideshow
+$(document).ready(function(){
+    $('.customer-logos').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 4
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 3
+            }
+        }]
+    });
+});
